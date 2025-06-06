320e029ae839d330a85cf0353b2b7a44672b1dd2
Uttam Kumar <uttammaurya377@gmail.com>
2025-06-06 09:22:47 +0530

    done

---
+++ 02_simple_values/main.go
@@ -0,0 +1,22 @@
+package main
+
+
+import "fmt"
+
+func main(){
+	//integers
+	// fmt.Println(1)
+	// fmt.Println(1+1)
+
+
+	//string
+	fmt.Println("Hello golang")
+
+
+	// boolean
+	fmt.Println((true))
+
+
+	// float
+	fmt.Println(1.2)
+}
