d274a55c16ae2bfd66b470ffa35ce7d57ade5b28
Uttam Kumar <uttammaurya377@gmail.com>
2025-06-15 18:22:33 +0530

    i am learning golang

--- 01_hello_world/main.go
+++
@@ -1,7 +0,0 @@
-package main             // Defines the main package — required for executables
-
-import "fmt"             // Imports the fmt package to use Println
-
-func main() {            // Entry point of the program
-    fmt.Println("Hello world")  // Prints "Hello world" to the console
-}
